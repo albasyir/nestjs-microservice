0cb0980d2bc538e7f4eb2e0df1a48c9d4f289cb2
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2025-04-21 23:19:34 +0700

    fix: redirect from root to docs

--- apps/order/src/app.module.ts
+++ apps/order/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DomainModule } from './domain/domain.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [InfrastructureModule, DomainModule],
   exports: [InfrastructureModule],
+  controllers: [AppController],
 })
 export class AppModule {}
